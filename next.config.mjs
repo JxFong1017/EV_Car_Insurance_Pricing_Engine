63ebacea0571b81fe61e11b3e0a51b045b035c34
JxFong1017 <jxfon1017@github.com>
2026-08-23 17:14:33 +0800

    deploy

--- next.config.mjs
+++ next.config.mjs
@@ -2,36 +2,27 @@
 const nextConfig = {
   reactStrictMode: true,
   rewrites: async () => {
-    return [
-      {
-        source: '/api/:path*',
-        destination:
-          process.env.NODE_ENV === 'development'
-            ? 'http://127.0.0.1:8000/api/:path*'
-            : '/api/',
-      },
-      {
-        source: '/health',
-        destination:
-          process.env.NODE_ENV === 'development'
-            ? 'http://127.0.0.1:8000/health'
-            : '/api/',
-      },
-      {
-        source: '/docs',
-        destination:
-          process.env.NODE_ENV === 'development'
-            ? 'http://127.0.0.1:8000/docs'
-            : '/api/',
-      },
-      {
-        source: '/openapi.json',
-        destination:
-          process.env.NODE_ENV === 'development'
-            ? 'http://127.0.0.1:8000/openapi.json'
-            : '/api/',
-      },
-    ];
+    if (process.env.NODE_ENV === 'development') {
+      return [
+        {
+          source: '/api/v1/:path*',
+          destination: 'http://127.0.0.1:8000/api/v1/:path*',
+        },
+        {
+          source: '/health',
+          destination: 'http://127.0.0.1:8000/health',
+        },
+        {
+          source: '/docs',
+          destination: 'http://127.0.0.1:8000/docs',
+        },
+        {
+          source: '/openapi.json',
+          destination: 'http://127.0.0.1:8000/openapi.json',
+        },
+      ];
+    }
+    return [];
   },
 };
 
